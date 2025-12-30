67e020d49379de1bd39c53fedab149dd77dfdcfd
Dtdeep <ddumagan@addu.edu.ph>
2025-12-30 16:33:40 +0800

    Solve repeatString Exercise

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function(stringW, repeatNo) {
+    if(repeatNo < 0){
+        return "ERROR";
+    }
+    let stringFinal = "";
+    for(let i = 0; i < repeatNo; i++){
+        stringFinal += stringW;
+    }
+    return stringFinal;
 
 };
+
+// a for loop that repeats n times. concatonate the string by itself by loop time
 
 // Do not edit below this line
 module.exports = repeatString;
