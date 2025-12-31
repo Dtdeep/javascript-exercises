10f4ab8634a4618682dabd146bc234b826ac504d
Dtdeep <ddumagan@addu.edu.ph>
2025-12-31 21:45:05 +0800

    Solve sumAll problem Exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(beginning, ending) {
+    if (beginning < 0 || ending < 0 || !Number.isInteger(beginning) || !Number.isInteger(ending)) return "ERROR";
+    let sum = 0;
+    let beginCondition = (beginning >= ending) ? ending : beginning;
+    let endCondition = (beginning >= ending) ? beginning : ending;
+    for(let i = beginCondition; i <= endCondition; i++){
+        sum += i;
+    }
+    return sum;
 };
+
+//create a variable sum = 0;
+//create a for loop, initial value is argument 1 and condition is until argument 2
+// sum += i;
+
 
 // Do not edit below this line
 module.exports = sumAll;
