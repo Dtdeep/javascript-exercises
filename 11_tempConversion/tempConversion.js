5aa9ee7c1ed0cfe330b38fbddbe5781927684754
Dtdeep <ddumagan@addu.edu.ph>
2026-01-01 22:16:52 +0800

    Solve tempConversion exercise Problem

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(Fahrenheit) {
+  const fahrenheitInCelsius = (Fahrenheit - 32) * (5/9);
+  const roundedFahrenheitInCelsius = Math.round(fahrenheitInCelsius * 10) / 10;
+  const fixedRoundedFahrenheitInCelsius = parseFloat(roundedFahrenheitInCelsius.toFixed(1));
+  
+  return fixedRoundedFahrenheitInCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function convertToFahrenheit(temperatureInCelsius) {
+  const celsiusInFahrenheit = (temperatureInCelsius * (9/5)) + 32;
+  const roundedCelsiusInFahrenheit = Math.round(celsiusInFahrenheit * 10) / 10;
+  const fixedRoundedCelsiusInFahrenheit = parseFloat(roundedCelsiusInFahrenheit.toFixed(1));
+ 
+  return fixedRoundedCelsiusInFahrenheit;
 };
 
 // Do not edit below this line
