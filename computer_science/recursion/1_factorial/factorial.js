199944708c402b6e4e8f8e7d1d03b0c3d01345a1
Dtdeep <dumagandesthiny@gmail.com>
2026-06-28 17:32:08 +0800

    Make factorial return the factorial of any number

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,11 @@
-const factorial = function() {
-
+const factorial = function (number) {
+  if (number < 0 || !Number.isInteger(number)) {
+    return undefined;
+  }
+  if (number == 0) {
+    return 1;
+  }
+  return number * factorial(number - 1);
 };
 
 // Do not edit below this line
