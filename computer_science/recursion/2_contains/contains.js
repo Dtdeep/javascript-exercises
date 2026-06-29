fb815e8264311f3db632bedfcbb8248f7c8ed6e0
Dtdeep <dumagandesthiny@gmail.com>
2026-06-29 20:32:29 +0800

    Add a function that determins if an element is in an object

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,20 @@
-const contains = function() {
-  
+const contains = function (objectPass, searchValue) {
+  let answer = false;
+  for (let attribute of Object.values(objectPass)) {
+    console.log("this is the attribute: ", attribute, typeof attribute);
+    if (typeof attribute === "object" && attribute !== null) {
+      answer = contains(attribute, searchValue);
+      if (answer) {
+        return true;
+      }
+    }
+    if (attribute === searchValue || Number.isNaN(searchValue)) {
+      console.log("found!");
+      return true;
+    }
+  }
+  return answer;
 };
-  
+
 // Do not edit below this line
 module.exports = contains;
