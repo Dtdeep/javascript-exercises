0c2cbbee19603b2210b4e68ce3fdd650850954bf
Dtdeep <dumagandesthiny@gmail.com>
2026-07-02 22:24:18 +0800

    Solved 4 and 5

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,6 +1,18 @@
-const pascal = function() {
-  
+const pascal = function (coordinate, resultArray = [1]) {
+  if (coordinate == 1) {
+    return resultArray;
+  }
+  resultArray.push(0);
+  resultArray.unshift(0);
+
+  let finalArray = [];
+
+  for (let i = 0; i < resultArray.length - 1; i++) {
+    const result = resultArray[i] + resultArray[i + 1];
+    finalArray.push(result);
+  }
+  return pascal(coordinate - 1, finalArray);
 };
-  
+
 // Do not edit below this line
 module.exports = pascal;
